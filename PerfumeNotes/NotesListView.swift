316e7f4883b1f3e8d34b910c04fa5924de2c9534
alcode111 <al.code.111@gmail.com>
2025-02-11 21:02:40 +0100

    Implement NavigationStack on the List and add filter feature

--- PerfumeNotes/NotesListView.swift
+++ PerfumeNotes/NotesListView.swift
@@ -9,28 +9,53 @@ import SwiftUI
 
 struct NotesListView: View {
     let notes = Notes()
+    
+    @State var searchText = ""
+    
+    var filteredNotes: [Note] {
+        if searchText.isEmpty {
+            return notes.notes
+        } else {
+            return notes.notes.filter { note in
+                note.name.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
+    
     var body: some View {
-        List(notes.notes) { note in
-            HStack(spacing: 16) {
-                Image(note.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: Color(.label), radius: 2)
-                
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(note.name)
-                        .fontWeight(.bold)
-                    
-                    Text(note.family.rawValue)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 16)
-                        .padding(.vertical, 4)
-                        .background(note.family.background)
-                        .clipShape(.capsule)
+        NavigationStack {
+            List(filteredNotes) { note in
+                NavigationLink {
+                    Image(note.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack(spacing: 16) {
+                        Image(note.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: Color(.label), radius: 2)
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text(note.name)
+                                .fontWeight(.bold)
+                            
+                            Text(note.family.rawValue)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 16)
+                                .padding(.vertical, 4)
+                                .background(note.family.background)
+                                .clipShape(.capsule)
+                        }
+                    }
                 }
             }
+            .navigationTitle("Notes")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
         }
     }
 }
