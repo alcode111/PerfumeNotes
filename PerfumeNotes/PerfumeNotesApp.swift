6fc0ba6d3366b847b0de6d4b01a0ca5eb50245a8
alcode111 <al.code.111@gmail.com>
2025-02-11 13:37:51 +0100

    Import XCode project files

---
+++ PerfumeNotes/PerfumeNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfumeNotesApp.swift
+//  PerfumeNotes
+//
+//  Created by Ismaïl on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PerfumeNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
