79073c8113a41f2cdba7f1381c9cd57b321e5c3f
alcode111 <al.code.111@gmail.com>
2025-02-12 14:36:15 +0100

    Enhance NoteDetailView with VStack, dynamic data from JSON and improve layout

--- PerfumeNotes/NoteDetailView.swift
+++ PerfumeNotes/NoteDetailView.swift
@@ -23,22 +23,75 @@ struct NoteDetailView: View {
                         .scaledToFit()
                         .frame(width: geo.size.width/1.5, height: geo.size.height/5)
                         .shadow(color: Color(.label), radius: 3)
-                        .offset(y: 80)
+                        .offset(y: 50)
                 }
                 
                 // Note name + scientific name
-                
-                // location name + location
-                
-                // Characteristics X
-                
-                // Pairings X
-                
-                // Fun fact
-                
-                // Method of extraction
-                
-                // Link
+                VStack(alignment: .leading) {
+                    Text(note.name)
+                        .font(.title)
+                        .bold()
+                    +
+                    Text("(\(note.scientificName))")
+                        .italic()
+                        .font(.title3)
+                        .bold()
+                    
+                    // Description
+                    Text(note.description)
+                        .font(.title3)
+                        .padding(.bottom, 32)
+                    
+                    // location name + location
+                    
+                    // Characteristics X
+                    Text("Main characteristics:")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                    
+                    ForEach(note.characteristics, id: \.self) { characteristic in
+                        Text("• \(characteristic)")
+                            .font(.subheadline)
+                    }
+                    
+                    // Pairings X
+                    Text("Usually paired with:")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .padding(.top, 16)
+                    
+                    ForEach(note.pairings, id: \.self) { pairing in
+                        Text("• \(pairing)")
+                            .font(.subheadline)
+                    }
+                    
+                    // Fun fact
+                    Text(note.funFact)
+                        .italic()
+                        .padding()
+                        .background(.gray.opacity(0.3))
+                        .clipShape(.rect(cornerRadius: 8))
+                        .padding(.vertical, 32)
+                    
+                    // Method of extraction
+                    Text("Method of extraction:")
+                        .underline()
+                        .fontWeight(.semibold)
+                    +
+                    Text(" \(note.extraction)")
+                    
+                    Text("Read more:")
+                        .font(.caption)
+                        .padding(.top, 16)
+
+                    // Link
+                    Link(note.link, destination: URL(string: note.link) ?? URL(string: "https://www.wikipedia.org/")!)
+                        .font(.caption)
+                }
+                .padding(.top, 32)
+                .padding(16)
+                .padding(.bottom)
+                .frame(width: geo.size.width, alignment: .leading)
             }
         }
         .ignoresSafeArea()
@@ -46,5 +99,5 @@ struct NoteDetailView: View {
 }
 
 #Preview {
-    NoteDetailView(note: Notes().notes[1])
+    NoteDetailView(note: Notes().notes[2])
 }
