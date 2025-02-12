31659d579b82e63dad3fc9256c1654f7169d2a08
alcode111 <al.code.111@gmail.com>
2025-02-12 12:01:38 +0100

    Start laying out basic UI for NoteDetailView

--- PerfumeNotes/NoteDetailView.swift
+++ PerfumeNotes/NoteDetailView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct NoteDetailView: View {
+    
+    let note: Note
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ScrollView {
+                ZStack(alignment: .bottomTrailing) {
+                    Image(note.family.rawValue.lowercased())
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Image(note.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width/1.5, height: geo.size.height/5)
+                        .shadow(color: Color(.label), radius: 3)
+                        .offset(y: 80)
+                }
+                
+                // Note name + scientific name
+                
+                // location name + location
+                
+                // Characteristics X
+                
+                // Pairings X
+                
+                // Fun fact
+                
+                // Method of extraction
+                
+                // Link
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
-    NoteDetailView()
+    NoteDetailView(note: Notes().notes[1])
 }
